becfaa02855cb5485f7ad10790298205f7bff08e
Joel Fernandes <joelf@ti.com>
2013-06-28 14:27:41 -0500

    Add SPI node for SPIFlash
    Signed-off-by: Joel Fernandes <joelf@ti.com>

--- drivers/mtd/devices/m25p80.c
+++ drivers/mtd/devices/m25p80.c
@@ -731,9 +731,6 @@ static const struct spi_device_id m25p_ids[] = {
 	{ "en25q64", INFO(0x1c3017, 0, 64 * 1024, 128, SECT_4K) },
 	{ "en25qh256", INFO(0x1c7019, 0, 64 * 1024, 512, 0) },
 
-	/* Everspin */
-	{ "mr25h256", CAT25_INFO(  32 * 1024, 1, 256, 2) },
-
 	/* GigaDevice */
 	{ "gd25q32", INFO(0xc84016, 0, 64 * 1024,  64, SECT_4K) },
 	{ "gd25q64", INFO(0xc84017, 0, 64 * 1024, 128, SECT_4K) },
