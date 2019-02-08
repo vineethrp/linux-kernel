90685d67c3d0a9401afb5e5a2e3a6cff1f1fd072
Joel Fernandes <joelaf@google.com>
2019-02-08 11:23:11 -0500

    refine test prog
    Signed-off-by: Joel Fernandes <joelaf@google.com>

--- test-set-name.c
+++ test-set-name.c
@@ -1,24 +1,10 @@
-/*
- * Example memfd_create(2) server application.
- *
- * Copyright (C) 2015 Ahmed S. Darwish <darwish.07@gmail.com>
- *
- * SPDX-License-Identifier: Unlicense
- *
- * Kindly check attached README.md file for further details.
- */
-
-#define _GNU_SOURCE
 #include <sys/syscall.h>
 #include <sys/mman.h>
 #include <sys/prctl.h>
-#include <sys/socket.h>
-#include <linux/un.h>
 
 #include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <time.h>
@@ -27,14 +13,115 @@
 #define PR_SET_VMA             0x53564d41
 #define PR_SET_VMA_ANON_NAME          0
 
+#define MAP_SIZE (300* 1024)
+
+#define DEBUG 1
+
+#ifdef DEBUG
+#define dprintf(fmt, args...)    fprintf(stderr, fmt, ## args)
+#else
+#define dprintf(fmt, args...)
+#endif
+
+void print_maps(void)
+{
+    FILE *fptr;
+    char c, filename[] = "/proc/self/maps";
+
+    fptr = fopen(filename, "r");
+    if (fptr == NULL) {
+        fprintf(stderr, "Cannot open file \n");
+        exit(1);
+    }
+
+    c = fgetc(fptr);
+    while (c != EOF) {
+        printf ("%c", c);
+        c = fgetc(fptr);
+    }
+
+    fclose(fptr);
+    return;
+
+}
+
+void print_smaps(void)
+{
+    FILE *fptr;
+    char c, filename[] = "/proc/self/smaps";
+
+    fptr = fopen(filename, "r");
+    if (fptr == NULL) {
+        fprintf(stderr, "Cannot open file \n");
+        exit(1);
+    }
+
+    c = fgetc(fptr);
+    while (c != EOF) {
+        printf ("%c", c);
+        c = fgetc(fptr);
+    }
+
+    fclose(fptr);
+    return;
+
+}
+
+/* Try to determine where in the addr space is there
+ * a large chunk of memory that can be MAP_FIXED mmaped
+ */
+unsigned long get_start_address(void)
+{
+    void *a;
+
+    a = malloc(1024 * 1024 * 1024); // 1GB
+    dprintf("allocated arena: %lx\n", (unsigned long)a);
+    free(a);
+
+    // Align start address to 4k boundary, or mmaps on it fail
+    return ((unsigned long)a & ~4095);
+}
+
+unsigned long last_end;
+
+void *create_map(void)
+{
+    unsigned long addr;
+    int map_flags = MAP_ANONYMOUS | MAP_PRIVATE;
+
+    // Separate start of a map from new map by a page
+    if (!last_end)
+        addr = get_start_address();
+    else
+        addr = last_end + (4096);
+
+    if (addr)
+        dprintf("last end was 0x%lx, new start 0x%lx\n",  last_end, addr);
+
+    void *m = mmap((void *)addr, MAP_SIZE, PROT_READ | PROT_WRITE, map_flags, -1, 0);
+
+    if (!m || m == MAP_FAILED) {
+        fprintf(stderr, "map creation failed, errno: %d\n", errno);
+    }
+
+    last_end = (unsigned long)m + MAP_SIZE;
+
+    dprintf("Created map: 0x%lx, updated last_end to 0x%lx\n", (unsigned long)m, last_end);
+    return m;
+}
+
 int main() {
-    char *shm;
+    char *shm, *shm1, *shm2, *shm3;
     int ret;
 
-    shm = mmap(NULL, 8192, PROT_READ | PROT_WRITE, MAP_ANONYMOUS|MAP_PRIVATE, -1, 0);
+    shm   = create_map();
+    shm1  = create_map();
+    shm2  = create_map();
+    shm3  = create_map();
 
-    ret = prctl(PR_SET_VMA, PR_SET_VMA_ANON_NAME, shm, 8192, (unsigned long)"test-name");
+    print_maps();
 
+    ret = prctl(PR_SET_VMA, PR_SET_VMA_ANON_NAME, shm, MAP_SIZE, (unsigned long)"test-name");
     printf("prctl ret: %d\n", ret);
 
     while (1);
