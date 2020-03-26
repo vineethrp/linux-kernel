868da02e23f20682099bf1f74fa4782295c466ac
vpillai <vpillai@digitalocean.com>
2020-03-26 19:50:55 +0000

    Revert sched_in_ipi in favor of core_priv/core_pause_pending

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -4369,7 +4369,6 @@ restart:
 #ifdef CONFIG_SCHED_CORE
 
 DEFINE_PER_CPU(bool, sched_core_priv);
-DEFINE_PER_CPU(bool, sched_in_ipi);
 static DEFINE_PER_CPU_SHARED_ALIGNED(call_single_data_t, htpause_csd);
 
 static inline bool cookie_equals(struct task_struct *a, unsigned long cookie)
@@ -4461,16 +4460,6 @@ redo_pause:
 	trace_printk("[unpriv] exit IPI\n");
 }
 
-void set_sched_in_ipi(void)
-{
-	smp_store_release(this_cpu_ptr(&sched_in_ipi), true);
-}
-
-void reset_sched_in_ipi(void)
-{
-	smp_store_release(this_cpu_ptr(&sched_in_ipi), false);
-}
-
 /*
  * Enter the privileged (exclusive) core state where the other HT
  * should be paused if it is running 'untrusted' code, until
@@ -4489,7 +4478,8 @@ void sched_core_priv_enter(void)
 	if (!sched_core_enabled(rq))
 		return;
 
-	if (this_cpu_read(sched_in_ipi))
+	// Return if we are in an IPI sent from the other sibling.
+	if (READ_ONCE(rq->core_pause_pending))
 		return;
 
 	// softirqs could get interrupted while we are in the exclusive state,
